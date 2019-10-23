e46f88f7c90f335c64939f691222096ffd4baa6c
Ammar Jabakji <ammarjabakji@gmail.com>
2019-10-23 20:48:43 +0300

    b

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-/* eslint-disable no-console */
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
-bookRouter.route("/book").get((req, res) => {
-  const response = { hello: "this is my API" };
-  res.json(response);
+bookRouter.route('/books').get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
 });
+bookRouter.route('/books/:bookId').get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
+});
+app.use('/api', bookRouter);
 
-app.use("/api", bookRouter);
-
-app.get("/", (req, res) => {
-  res.send(" Welcome to my api");
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
 });
 
 app.listen(port, () => {
-  console.log(` Server started on port  ${port}`);
+  console.log(`Running on port ${port}`);
 });
